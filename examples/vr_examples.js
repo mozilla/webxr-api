541e52b9f4cf9f00d6451e7c8c98d6c8590482b6
Trevor F. Smith <trevor@trevor.smith.name>
2017-08-07 10:52:35 -0700

    Update the VR example to use the API changes.

--- examples/vr_examples.js
+++ examples/vr_examples.js
@@ -3,13 +3,17 @@ class VRSetupExample {
 	constructor(){
 		this.display = null
 		this.session = null
+		this.realityLayer = null
 
+		// Find a display
 		navigator.XR.getDisplays().then(displays => {
 			if(displays.length == 0) {
-				console.log('No displays are available')
+				console.error('No displays are available')
 				return
 			}
 			this.display = displays[0] // production code would allow the user to choose
+
+			// Request a session
 			this.display.requestSession({ exclusive: true }).then(session => {
 				this.handleNewSession(session)
 			}).catch(err => {
@@ -20,17 +24,34 @@ class VRSetupExample {
 
 	handleNewSession(session){
 		this.session = session
-		this.session.requestRealityChange(this.session.createEmptyReality()).then(changed => {
+
+		// Create an empty Reality in which to create our VR
+		const reality = this.session.createEmptyReality('VR Example', false)
+
+		// Request that this session use the new Reality
+		this.session.requestRealityChange(reality).then(changed => {
 			if(changed === false){
 				console.error('Could not change realities')
 				return
 			}
+
+			// Request the Reality's XRLayer so that we can render into it
+			reality.requestLayer().then(layer => {
+				// because this script context created it, layer will always be non-null
+				this.realityLayer = layer
+			})
+
+			// Now start requesting frames
 			this.session.requestFrame(frame => { this.handleFrame(frame) })
 		})
 	}
 
 	handleFrame(frame){
+		// Request the frame after this one
 		this.session.requestFrame(frame => { this.handleFrame(frame) })
-		// render into this.session.layer.context
+
+		// Render into this.realityLayer.context using a WebGL lib like Three.js
+
+		// Render into this.session.layer.context using a WebGL lib like Three.js
 	}
 }
